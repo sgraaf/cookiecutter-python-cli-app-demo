335b9b0dd5558d64f8e2693a815e2a6ae0a16d52
Automated <actions@users.noreply.github.com>
2024-06-12 23:50:37 +0000

    Demo of cookiecutter-python-cli-app @ 10d1389

---
+++ tests/test_cli.py
@@ -0,0 +1,38 @@
+from importlib import import_module
+from importlib.metadata import version
+
+import pytest
+from click.testing import CliRunner
+
+from cookiecutter_python_cli_app_demo.cli import cli
+
+from .utils import run_command_in_shell
+
+
+@pytest.fixture()
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+def test_main_module() -> None:
+    """Exercise (most of) the code in the `__main__` module."""
+    import_module("cookiecutter_python_cli_app_demo.__main__")
+
+
+def test_run_as_module() -> None:
+    """Is the script runnable as a Python module?"""
+    result = run_command_in_shell("python -m cookiecutter_python_cli_app_demo --help")
+    assert result.exit_code == 0
+
+
+def test_run_as_executable() -> None:
+    """Is the script installed (as a `console_script`) and runnable as an executable?"""
+    result = run_command_in_shell("cookiecutter-python-cli-app-demo --help")
+    assert result.exit_code == 0
+
+
+def test_version_runner(runner: CliRunner) -> None:
+    """Does `--version` display the correct version?"""
+    result = runner.invoke(cli, ["--version"])
+    assert result.exit_code == 0
+    assert result.output == f"cli, version {version('cookiecutter-python-cli-app-demo')}\n"
